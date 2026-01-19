4dc8e31d1a7e9a9ed92079636ebbcd6d57a0925d
Muthuu SVS <musv6057@colorado.edu>
2026-01-19 13:28:49 -0500

    implemented finder.sh

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #finder script for assignment 1
 #Author: Muthuu SVS
 
@@ -9,6 +9,7 @@ set -u
 FILESDIR=$1 # first argument directory path
 SEARCHSTR=$2 #2nd argument text string to search for
 
+#checks if exactly 2 arguments were passed
 if [[ "$#" -ne 2 ]]
 then
     echo "Error: 2 arguments not supplied." >&2
@@ -18,3 +19,15 @@ fi
 
 echo "Two arguments passed in."
 
+#checks if filedirectory is a valid directory
+if [ -d "$FILESDIR" ]
+then
+    echo "Directory is valid"
+else
+    echo "Error: Directory not valid" >&2
+    exit 1
+fi
+
+FILECOUNT=$(find $FILESDIR -type -d | wc -l)
+LINECOUNT=$(grep -r $SEARCHSTR $FILESDIR | wc -l)
+echo "Number of files are $FILECOUNT and the number of matching lines are $LINECOUNT" 
